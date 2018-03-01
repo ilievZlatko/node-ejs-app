0f4fea3dcf81cccb02a8bf4c3c31ebf96c632cd9
zlatkoIliev <iliev.zlatko@gmail.com>
2018-03-01 19:46:14 +0200

    commit current progress

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const mongoose = require('mongoose');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+mongoose.connect('mongodb://localhost/nodekb');
+const db = mongoose.connection;
+
+app.use(express.static(__dirname + '/assets'));
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Home Route
+app.get('/', (req, res) => {
+    let articles = [
+        {
+            id: 1,
+            title: 'Article One',
+            author: 'John Doe',
+            body: 'This is Article One'
+        },
+        {
+            id: 2,
+            title: 'Article Two',
+            author: 'John Doe',
+            body: 'This is Article Two'
+        },
+        {
+            id: 3,
+            title: 'Article Three',
+            author: 'Philip Baker',
+            body: 'This is Article Three'
+        }
+    ];
+    res.render('index', { title: 'Hello', articles: articles });
+});
+
+app.get('/articles/add', (req, res) => {
+    res.render('add_article', { title: 'Add Article' });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('Server is running on port: ' + PORT);
+});
